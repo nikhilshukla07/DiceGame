da3424b5d7f1447a5d85d391d34725369d80b3d0
nikhilshukla07 <snikhilshukla@gmail.com>
2020-08-19 18:36:47 +0530

    added score

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var roll=document.getElementById("roll")
+
+
+
+
+var roll=document.getElementById("roll");
+var score1=0;
+var score2=0;
 roll.onclick=
 function()
 {
@@ -8,21 +14,25 @@ function()
     document.getElementById("dice2").firstElementChild.src="images/dice"+num2+".png";
     if(num1>num2)
     {
+        score1++;
         document.getElementById("result").innerText="Player 1 Wins";
         document.querySelector("#player1 h1").classList.add("players");
         document.querySelector("#player2 h1").classList.remove("players");
+        document.querySelector("#score1 h1").textContent=score1;
+  
     }
     else if(num1<num2)
     {
+        score2++;
         document.getElementById("result").innerText="Player 2 Wins";
         document.querySelector("#player1 h1").classList.remove("players");
         document.querySelector("#player2 h1").classList.add("players"); 
+        document.querySelector("#score2 h1").textContent=score2;
     }
     else
     {
         document.getElementById("result").innerText="It's a tie";
         document.querySelector("#player1 h1").classList.add("players");
         document.querySelector("#player2 h1").classList.add("players");
-    
     }
 }
